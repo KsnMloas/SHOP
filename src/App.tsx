d239d0222d11c95e4952f843008c1ca7f7dd0255
KsnMloas <ertrtder@gmail.com>
2026-02-10 06:09:30 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='Premiera'>
+        <p className='Premiera-Text'>
+          Updated Webiste Kernel X 2026.05.25
+        </p>
+        <span className='Creator'>
+          [* : Kernel_0EX]
+        </span>
+      </div>
+
+    </>
+  )
+}
+
+export default App
